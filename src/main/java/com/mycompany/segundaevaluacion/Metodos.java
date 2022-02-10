275bf70a006fb4d17bc8ef47dcdbbaecac3ac6a0
Lcordova-prog <lcordova.7915@unimar.edu.ve>
2022-02-10 17:13:29 -0400

    Listo la clase Revolver

--- src/main/java/com/mycompany/segundaevaluacion/Metodos.java
+++ src/main/java/com/mycompany/segundaevaluacion/Metodos.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.segundaevaluacion;
 
-/**
- *
- * @author Pc1
- */
+
 public class Metodos {
-    
+    public static int generarNumeroAleatorios(int minimo, int maximo){
+    int num = (int) Math.floor(Math.random())*(maximo - minimo + 1) + (minimo);
+        return num;    
+    }
 }
--- src/main/java/com/mycompany/segundaevaluacion/Revolver.java
+++ src/main/java/com/mycompany/segundaevaluacion/Revolver.java
@@ -5,8 +5,37 @@ package com.mycompany.segundaevaluacion;
 public class Revolver {
     private int PosicionDisparoActual;
         private int PosicionDisparo;
+    
         
         public Revolver(){
+            PosicionDisparoActual = Metodos.generarNumeroAleatorios(1, 6);
+            PosicionDisparo = Metodos.generarNumeroAleatorios(1, 6);
+        }
+        
+        public boolean disparar(){
             
+            boolean Fuego = false;
+            
+            if (PosicionDisparoActual == PosicionDisparo) {
+                
+                Fuego = true;
+            }
+            
+            siguienteTiro();
+            
+            
+            return Fuego;
+        }
+        
+        
+        public void siguienteTiro(){
+            if (PosicionDisparoActual == 6) {
+                PosicionDisparoActual = 1;
+        }else{
+                PosicionDisparoActual ++;
+            }
+        }
+        public String String(){
+            return "Posicion de la Bala Actual: "+ PosicionDisparoActual + ", Sitio de la Bala"+ PosicionDisparo;
         }
 }
