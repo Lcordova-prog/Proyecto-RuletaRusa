c7f3a240ecea7ba5475eab7f67c396e733206848
Lcordova-prog <lcordova.7915@unimar.edu.ve>
2022-02-10 17:32:53 -0400

    Se ha programado la clase Participante

--- src/main/java/com/mycompany/segundaevaluacion/Participantes.java
+++ src/main/java/com/mycompany/segundaevaluacion/Participantes.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.segundaevaluacion;
 
-/**
- *
- * @author Pc1
- */
+
 public class Participantes {
+    private int usuario;
+    private String nombre;
+    private boolean vivo;
+    
+    public Participantes(int id){
+        this.usuario = id;
+        this.nombre = "Participante"+id;
+        this.vivo = true;
+    }
+    public boolean Fuego(Revolver r){
+        System.out.println("EL" +nombre+" apunta con el revolver al siguiente jugador");
+        if (r.disparar()) {
+            this.vivo = false;
+            System.out.println("EL"+nombre+"ha matado... al"+nombre);
+        }else{
+            System.out.println("El"+nombre+"se ha salvado....");
+        }
+        return vivo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
     
 }
