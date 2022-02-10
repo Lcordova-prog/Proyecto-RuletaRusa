266f2d7c5e9db673987040bd9ac58ce8dafd8a43
Lcordova-prog <lcordova.7915@unimar.edu.ve>
2022-02-10 17:06:11 -0400

    Estoy creando la clase Revolver

---
+++ src/main/java/com/mycompany/segundaevaluacion/Revolver.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.segundaevaluacion;
+
+
+public class Revolver {
+    private int PosicionDisparoActual;
+        private int PosicionDisparo;
+        
+        public Revolver(){
+            
+        }
+}
